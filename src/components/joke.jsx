88be2325494ddffb9cd2396b162340de2b7b78a1
mukunda001 <mukunda8702@gmail.com>
2026-07-07 18:31:22 +0545

    Mappint the Components

---
+++ src/components/joke.jsx
@@ -0,0 +1,10 @@
+
+export default function joke(props){
+    return(
+       <>
+       {props.setup && <p className = "setup" > Setup: {props.setup} </p>}
+       <p className = "punchline"> Punchline: {props.punchline} </p>
+       <hr/>
+       </>
+    )   
+}
